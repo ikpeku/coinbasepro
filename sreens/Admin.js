15bb8b55ecc1af96525b861be7131dccfbc32ea7
ikpeku <ikpedaniel07@gmail.com>
2023-04-05 01:45:11 +0100

    delete user implemented on admin

--- sreens/Admin.js
+++ sreens/Admin.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import {View, Text,  Button, ActivityIndicator} from 'react-native';
-import { SafeAreaView ,} from 'react-native-safe-area-context';
+import { View, Text, Button, ActivityIndicator, Alert } from 'react-native';
+import { SafeAreaView, } from 'react-native-safe-area-context';
 
-import {collection, query, getDocs} from "firebase/firestore"
-import {db} from "../firebase/firebaseConfig"
+import { collection, query, getDocs, deleteDoc, doc } from "firebase/firestore"
+import { db } from "../firebase/firebaseConfig"
 import { FlashList } from '@shopify/flash-list';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
-const Admin = ({navigation}) => {
+const Admin = ({ navigation }) => {
   const [users, setUsers] = useState(null)
   const [isLoading, setIsLoding] = useState(false)
 
+
   useEffect(() => {
-    const fetchUser = async() => {
+    const fetchUser = async () => {
       setIsLoding(true)
 
       try {
@@ -26,16 +27,16 @@ const Admin = ({navigation}) => {
         let AllUsers = []
 
         querySnapShot.forEach((doc) => {
-        
-          return AllUsers.push({id: doc.id, users: doc.data()})
+
+          return AllUsers.push({ id: doc.id, users: doc.data() })
         })
 
         setUsers(AllUsers)
 
         setIsLoding(false)
-        
+
       } catch (error) {
-        
+
       }
 
     }
@@ -44,46 +45,84 @@ const Admin = ({navigation}) => {
   }, [])
 
 
-   if (isLoading) {
+  const onDeletePress = (item) => {
+
+    const { id, users } = item
+
+
+    Alert.alert('Delete user', `do you want to delete ${users.Name}`, [
+      {
+        text: 'No',
+        // onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: 'Yes', onPress: () => deleted(id)
+
+
+      },
+    ]);
+  }
+
+  const deleted = async (id) => {
+
+
+    const userRef = doc(db, "users", id)
+
+    try {
+      await deleteDoc(userRef)
+      setUsers(users.filter(user => user.id !== id))
+    } catch (error) {
+      Alert.alert("Delete user", "operation failed try again")
+      // console.log(error.message)
+    }
+  }
+
+
+
+
+  if (isLoading) {
     return (
-      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <ActivityIndicator size={'large'}  color="#3376bc" />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size={'large'} color="#3376bc" />
       </View>
     )
   }
 
 
-return(
-  <SafeAreaView style={{flex: 1}}>
-      
-    
-    <View>
-    <Text style={{textAlign: "center", marginTop: 20, fontWeight: "bold", fontSize: 25, fontFamily: 'Nunito-Medium', color:"#3376bc"}}>Dashboard</Text>
-    </View>
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
 
 
-    <View>
-      <Text style={{fontWeight: "bold", fontSize: 20, fontFamily: 'Nunito-Medium', color:"#3376bc", borderBottomWidth: 1}}>Users</Text>
-      {/* <Button title="goto user" onPress={() => navigation.navigate("user")} /> */}
+      <View>
+        <Text style={{ textAlign: "center", marginTop: 20, fontWeight: "bold", fontSize: 25, fontFamily: 'Nunito-Medium', color: "#3376bc" }}>Dashboard</Text>
+      </View>
 
-    </View>
 
-    <View style={{flex: 1}}>
-    <FlashList
-      estimatedItemSize={10}
-        // keyExtractor={(item) => item.id}
-        data={users}
-        renderItem={({ item }) => (
-          <View>
-            <Pressable onPress={() => navigation.navigate("user", {user: item.users, id: item.id})}>
-            <Text style={{color: "#3376bc", margin: 5, fontSize: 22, fontWeight: "bold"}}>{item.users.Name}</Text>
-            </Pressable>
-          </View>
-        )}
-      />
-    </View>
-  </SafeAreaView>
-)
+      <View>
+        <Text style={{ fontWeight: "bold", fontSize: 20, fontFamily: 'Nunito-Medium', color: "#3376bc", borderBottomWidth: 1, paddingHorizontal: 10 }}>Users</Text>
+
+      </View>
+
+      <View style={{ flex: 1 }}>
+        <FlashList
+          estimatedItemSize={10}
+          // keyExtractor={(item) => item.id}
+          data={users}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: "row", justifyContent: "space-between", marginVertical: 3, marginHorizontal: 10, alignItems: "center", borderWidth: 1, paddingLeft: 4 }} >
+
+              <Pressable onPress={() => navigation.navigate("user", { user: item.users, id: item.id })}>
+                <Text style={{ color: "#3376bc", fontSize: 22, fontWeight: "bold" }}>{item.id === "FmWNlv1V8LPy27CaEbCiannFw8Z2" ? "Admin" : item.users.Name}</Text>
+              </Pressable>
+
+              <Button disabled={item.id === "FmWNlv1V8LPy27CaEbCiannFw8Z2"} onPress={() => onDeletePress(item)} title='delete' />
+            </View>
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default Admin
--- sreens/User.js
+++ sreens/User.js
@@ -1,15 +1,15 @@
 import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
-import React, { useState , useEffect} from 'react';
-import { StyleSheet,View, Text, Image, Button, TextInput , Alert} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Image, Button, TextInput, Alert } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { db } from '../firebase/firebaseConfig';
 
-const User = ({route}) => {
+const User = ({ route }) => {
 
- 
 
-  const {Name,DOB, Email, Register, accountNumber, bankName, licence,passport, phoneNumber, licenceBack, swiftCode, Deposit, Withdraw, selectedCoin} = route.params?.user
+
+  const { Name, DOB, Email, Register, accountNumber, bankName, licence, passport, phoneNumber, licenceBack, swiftCode, Deposit, Withdraw, selectedCoin } = route.params?.user
 
 
 
@@ -22,16 +22,18 @@ const User = ({route}) => {
   const [USDT, setUSDT] = useState("")
 
 
-  const handleSelected = async() => {
+  const handleSelected = async () => {
     const userRef = doc(db, "users", route?.params?.id)
-   
-    try {
-      await updateDoc(userRef, {selectedCoin: {
-        amount: selectedCoinAmount,
-       }})
 
-       Alert.alert("done")
-      
+    try {
+      await updateDoc(userRef, {
+        selectedCoin: {
+          amount: selectedCoinAmount,
+        }
+      })
+
+      Alert.alert("done")
+
     } catch (error) {
       Alert.alert("error occurred try again")
     }
@@ -41,209 +43,215 @@ const User = ({route}) => {
   useEffect(() => {
 
     const refDoc = doc(db, "users", route?.params?.id)
-  
-      const unsub = onSnapshot(refDoc, (snapshot) => {
-        if(snapshot.exists()){
-          const {BNB, BTC, ETH, LTC, USDT} = snapshot?.data()?.token
-          setBNB(BNB)
-          setBTC(BTC)
-          setETH(ETH)
-          setLTC(LTC)
-          setUSDT(USDT)
 
-          const {amount} = snapshot?.data()?.selectedCoin
-          setSeleted(amount)
+    const unsub = onSnapshot(refDoc, (snapshot) => {
+      if (snapshot.exists()) {
+        const { BNB, BTC, ETH, LTC, USDT } = snapshot?.data()?.token
+        setBNB(BNB)
+        setBTC(BTC)
+        setETH(ETH)
+        setLTC(LTC)
+        setUSDT(USDT)
 
-        }
-      })
-  
-      return () => unsub()
+        const { amount } = snapshot?.data()?.selectedCoin
+        setSeleted(amount)
+
+      }
+    })
+
+    return () => unsub()
   }, [])
 
 
-  const verifyDeposit = async() => {
+  const verifyDeposit = async () => {
     const userRef = doc(db, "users", route?.params?.id)
-        
-        try {
-          await updateDoc(userRef, {Deposit: {
-            status: false,
-           }})
-  
-           Alert.alert("done")
-          
-        } catch (error) {
-          Alert.alert("error occurred try again")
+
+    try {
+      await updateDoc(userRef, {
+        Deposit: {
+          status: false,
         }
+      })
+
+      Alert.alert("done")
+
+    } catch (error) {
+      Alert.alert("error occurred try again")
+    }
 
   }
 
-  const verifyWithdraw = async() => {
-    const userRef = doc(db, "users", route?.params?.id)
-   try {
-    await updateDoc(userRef, {Withdraw: {
-      status: false,
-     }})
-     Alert.alert("done")
-   } catch (error) {
-    Alert.alert("error occurred try again")
-   }
-  }
-
-
-  const verifyToken = async() => {
+  const verifyWithdraw = async () => {
     const userRef = doc(db, "users", route?.params?.id)
     try {
-     await updateDoc(userRef, {token: {
-       BTC: BTC, BNB: BNB, LTC: LTC, USDT: USDT, ETH: ETH
-      }})
+      await updateDoc(userRef, {
+        Withdraw: {
+          status: false,
+        }
+      })
       Alert.alert("done")
     } catch (error) {
-     Alert.alert("error occurred try again")
+      Alert.alert("error occurred try again")
     }
-   }
-   
+  }
 
 
-return(
-  <SafeAreaView style={{marginHorizontal: 5}}>
-    <ScrollView>
-    <Text style={styles.Text}>Name: {Name}</Text>
-    <Text style={styles.Text}>D.O.B. : {DOB.toDate().toLocaleDateString()}</Text>
-    <Text style={styles.Text}>Email: {Email}</Text>
-    <Text style={styles.Text}>Register: {Register.toDate().toLocaleDateString()}</Text>
-    <Text style={styles.Text}>Account Number: {accountNumber}</Text>
-    <Text style={styles.Text}>Bank Name: {bankName}</Text>
-    <Text style={styles.Text}>phone Number: {phoneNumber}</Text>
-    <Text style={styles.Text}>swift code: {swiftCode}</Text>
-
-
-    {licence && <View style={{marginVertical: 20}}>
-       <Text style={styles.Text}>Licence front: </Text>
-       <Image source={{ uri: licence }} style={{ height: 200 , margin: 5}} />
-    </View>}
-
-    {licenceBack && <View style={{marginVertical: 20}}>
-       <Text style={styles.Text}>licenceBack: </Text>
-       <Image source={{ uri: licenceBack}} style={{ height: 200 , margin: 5}} />
-    </View>}
-
-    {passport && <View style={{marginVertical: 20}}>
-       <Text style={styles.Text}>passport: </Text>
-       <Image source={{ uri: passport}} style={{ height: 200 , margin: 5}} />
-    </View>}
-    
-    
-    
-
-
-    <Text style={[styles.Text,{color: "green", marginTop: 10}]}>Deposit:</Text>
-    <Text style={styles.Text}>status: {Deposit.status ? "Requestetind verification": "Has been verify"}</Text>
-    <Text style={styles.Text}>Deposit amount: {Deposit.amount}</Text>
-  
-    <Button
-  onPress={verifyDeposit}
-  disabled={!Deposit.status}
-  title="Verify deposit"
-  color="#3376bc"
-  accessibilityLabel="Learn more about this blue button"
-/>
-
-    <Text style={[styles.Text,{color: "green", marginTop: 30}]}>Withdrawal:</Text>
-    <Text style={styles.Text}>status: {Withdraw.status ? "Requestetind verification": "settle"}</Text>
-    <Text style={styles.Text}>Withdraw amount: {Withdraw.amount}</Text>
-    <Text style={styles.Text}>Withdraw to Address: {Withdraw.Address}</Text>
-    <Button
-  onPress={verifyWithdraw}
-  disabled={!Withdraw.status}
-  title="Verify withdrawal"
-  color="#3376bc"
-  accessibilityLabel="Learn more about this blue button"
-/>
-
-
-<View style={{flexDirection: "row", marginVertical: 12}}>
-          <Text style={{fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium"}}>Selected Coin: </Text>
-          <View style={{flex: 1}}>
-          <TextInput value={selectedCoinAmount} onChangeText={(text) => setSeleted(text)}  multiline={true} style={{marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5}}  />
-          
-          <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium" , textAlign: "center"}}> {selectedCoin.name}</Text>
-  
-          <Button
-  onPress={handleSelected}
-  // disabled={!Withdraw.status}
-  title="update balance"
-  color="#3376bc"
-  accessibilityLabel="Learn more about this blue button"
-/>
-
-          </View>
-        </View>
+  const verifyToken = async () => {
+    const userRef = doc(db, "users", route?.params?.id)
+    try {
+      await updateDoc(userRef, {
+        token: {
+          BTC: BTC, BNB: BNB, LTC: LTC, USDT: USDT, ETH: ETH
+        }
+      })
+      Alert.alert("done")
+    } catch (error) {
+      Alert.alert("error occurred try again")
+    }
+  }
 
 
 
-<View style={{marginBottom: 50}}>
-<Text style={[styles.Text,{color: "green", marginTop: 30}]}>Token: </Text>
-    
-       <View style={{flexDirection: "row", marginVertical: 12}}>
-          <Text style={{fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium"}}>BNB : </Text>
-          <View style={{flex: 1}}>
-          <TextInput value={BNB} onChangeText={(text) => setBNB(text)}  multiline={true} style={{marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5}}  />
-          <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium" , textAlign: "center"}}>BNB Available: {BNB}</Text>
-          </View>
-        </View>
-
-        <View style={{flexDirection: "row", marginVertical: 12}}>
-          <Text style={{fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium"}}>BTC : </Text>
-          <View style={{flex: 1}}>
-          <TextInput value={BTC} onChangeText={(text) => setBTC(text)} multiline={true} style={{marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5}}  />
-          <Text style={{fontSize: 18, fontFamily: "Nunito-Medium" , textAlign: "center"}}>BTC Available: {BTC}</Text>
-          </View>
-        </View>
+  return (
+    <SafeAreaView style={{ marginHorizontal: 5 }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.Text}>Name: {Name}</Text>
+        <Text style={styles.Text}>D.O.B. : {DOB.toDate().toLocaleDateString()}</Text>
+        <Text style={styles.Text}>Email: {Email}</Text>
+        <Text style={styles.Text}>Register: {Register.toDate().toLocaleDateString()}</Text>
+        <Text style={styles.Text}>Account Number: {accountNumber}</Text>
+        <Text style={styles.Text}>Bank Name: {bankName}</Text>
+        <Text style={styles.Text}>phone Number: {phoneNumber}</Text>
+        <Text style={styles.Text}>swift code: {swiftCode}</Text>
 
 
-        <View style={{flexDirection: "row", marginVertical: 12}}>
-          <Text style={{fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium"}}>ETH : </Text>
-          <View style={{flex: 1}}>
-          <TextInput value={ETH} onChangeText={(text) => setETH(text)} multiline={true} style={{marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5}}  />
-          <Text style={{fontSize: 18, fontFamily: "Nunito-Medium" , textAlign: "center"}}>ETH Available: {ETH}</Text>
-          </View>
-        </View>
+        {licence && <View style={{ marginVertical: 20 }}>
+          <Text style={styles.Text}>Licence front: </Text>
+          <Image source={{ uri: licence }} style={{ height: 200, margin: 5 }} />
+        </View>}
+
+        {licenceBack && <View style={{ marginVertical: 20 }}>
+          <Text style={styles.Text}>licenceBack: </Text>
+          <Image source={{ uri: licenceBack }} style={{ height: 200, margin: 5 }} />
+        </View>}
+
+        {passport && <View style={{ marginVertical: 20 }}>
+          <Text style={styles.Text}>passport: </Text>
+          <Image source={{ uri: passport }} style={{ height: 200, margin: 5 }} />
+        </View>}
 
 
-        <View style={{flexDirection: "row", marginVertical: 12}}>
-          <Text style={{fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium"}}>LTC : </Text>
-          <View style={{flex: 1}}>
-          <TextInput value={LTC} onChangeText={(text) => setLTC(text)} multiline={true} style={{marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5}}  />
-          <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center"}}>LTC Available: {LTC} </Text>
-          </View>
-        </View>
 
 
-        <View style={{flexDirection: "row", marginVertical: 12}}>
-          <Text style={{fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium"}}>USDT: </Text>
-          <View style={{flex: 1}}>
-          <TextInput value={USDT} onChangeText={(text) => setUSDT(text)} multiline={true} style={{marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5}}  />
-          <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center"}}>USDT Available: {USDT}</Text>
-          </View>
-        </View>
+
+        <Text style={[styles.Text, { color: "green", marginTop: 10 }]}>Deposit:</Text>
+        <Text style={styles.Text}>status: {Deposit.status ? "Requestetind verification" : "Has been verify"}</Text>
+        <Text style={styles.Text}>Deposit amount: {Deposit.amount}</Text>
+
         <Button
-  onPress={verifyToken}
-  // disabled={!Withdraw.status}
-  title="Set Token"
-  color="#3376bc"
-  accessibilityLabel="Learn more about this blue button"
-/>
+          onPress={verifyDeposit}
+          disabled={!Deposit.status}
+          title="Verify deposit"
+          color="#3376bc"
+          accessibilityLabel="Learn more about this blue button"
+        />
 
-</View>
+        <Text style={[styles.Text, { color: "green", marginTop: 30 }]}>Withdrawal:</Text>
+        <Text style={styles.Text}>status: {Withdraw.status ? "Requestetind verification" : "settle"}</Text>
+        <Text style={styles.Text}>Withdraw amount: {Withdraw.amount}</Text>
+        <Text style={styles.Text}>Withdraw to Address: {Withdraw.Address}</Text>
+        <Button
+          onPress={verifyWithdraw}
+          disabled={!Withdraw.status}
+          title="Verify withdrawal"
+          color="#3376bc"
+          accessibilityLabel="Learn more about this blue button"
+        />
 
 
-{/* <View>
+        <View style={{ flexDirection: "row", marginVertical: 12 }}>
+          <Text style={{ fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium" }}>Selected Coin: </Text>
+          <View style={{ flex: 1 }}>
+            <TextInput value={selectedCoinAmount} onChangeText={(text) => setSeleted(text)} multiline={true} style={{ marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5 }} />
+
+            <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center" }}> {selectedCoin.name}</Text>
+
+            <Button
+              onPress={handleSelected}
+              // disabled={!Withdraw.status}
+              title="update balance"
+              color="#3376bc"
+              accessibilityLabel="Learn more about this blue button"
+            />
+
+          </View>
+        </View>
+
+
+
+        <View style={{ marginBottom: 50 }}>
+          <Text style={[styles.Text, { color: "green", marginTop: 30 }]}>Token: </Text>
+
+          <View style={{ flexDirection: "row", marginVertical: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium" }}>BNB : </Text>
+            <View style={{ flex: 1 }}>
+              <TextInput value={BNB} onChangeText={(text) => setBNB(text)} multiline={true} style={{ marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5 }} />
+              <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center" }}>BNB Available: {BNB}</Text>
+            </View>
+          </View>
+
+          <View style={{ flexDirection: "row", marginVertical: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium" }}>BTC : </Text>
+            <View style={{ flex: 1 }}>
+              <TextInput value={BTC} onChangeText={(text) => setBTC(text)} multiline={true} style={{ marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5 }} />
+              <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center" }}>BTC Available: {BTC}</Text>
+            </View>
+          </View>
+
+
+          <View style={{ flexDirection: "row", marginVertical: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium" }}>ETH : </Text>
+            <View style={{ flex: 1 }}>
+              <TextInput value={ETH} onChangeText={(text) => setETH(text)} multiline={true} style={{ marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5 }} />
+              <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center" }}>ETH Available: {ETH}</Text>
+            </View>
+          </View>
+
+
+          <View style={{ flexDirection: "row", marginVertical: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium" }}>LTC : </Text>
+            <View style={{ flex: 1 }}>
+              <TextInput value={LTC} onChangeText={(text) => setLTC(text)} multiline={true} style={{ marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5 }} />
+              <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center" }}>LTC Available: {LTC} </Text>
+            </View>
+          </View>
+
+
+          <View style={{ flexDirection: "row", marginVertical: 12 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 18, fontFamily: "Nunito-Medium" }}>USDT: </Text>
+            <View style={{ flex: 1 }}>
+              <TextInput value={USDT} onChangeText={(text) => setUSDT(text)} multiline={true} style={{ marginLeft: 10, fontSize: 18, fontFamily: "Nunito-Medium", flex: 1, backgroundColor: "#fff", borderRadius: 10, color: "#000", paddingHorizontal: 15, paddingVertical: 5 }} />
+              <Text style={{ fontSize: 18, fontFamily: "Nunito-Medium", textAlign: "center" }}>USDT Available: {USDT}</Text>
+            </View>
+          </View>
+          <Button
+            onPress={verifyToken}
+            // disabled={!Withdraw.status}
+            title="Set Token"
+            color="#3376bc"
+            accessibilityLabel="Learn more about this blue button"
+          />
+
+        </View>
+
+
+        {/* <View>
 <Text style={[styles.Text,{color: "green", marginTop: 30}]}>Transaction:</Text>
 </View> */}
-    
-    </ScrollView>
-  </SafeAreaView>
-)
+
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
